abef6f457c92c2e954712cf0e1db7c3952dfee0a
Pedro J. Perez <pedro.j.perez@uv.es>
2018-02-07 23:11:06 +0100

    a github a ver

---
+++ R/data_INE.R
@@ -0,0 +1,34 @@
+#------------------------------ This file documents INE datasets
+
+#' Poblacion PADRON INE (SPAIN) 210-2017
+#'
+#' CPoblacion PADRON INE (SPAIN) 210-2017
+#' 1.219.020 registros
+#' 13 variables
+#'
+#' @source \url{http://www.ine.es}
+#'
+#' @format A data frame with 1.219.020 registros and 13 variables:
+#' \itemize{
+#'   \item X1: Microdato original
+#'   \item anyo: periodo
+#'   \item Code_Unidad_poblacional: Pancrudo = 44177000000
+#'   \item Nombre_Unidad_poblacional: PANCRUDO
+#'   \item Poblacion_Total: Poblacion Total
+#'   \item Pobalcion_H: Hombres
+#'   \item Poblacion_M: Mujeres
+#'   \item Provincia: PANCRUDO esta en Teruel = 44
+#'   \item Municipio: Codigo INE municipio Pancrudo = 177
+#'   \item Entidad_colectiva:
+#'   \item Entidad_singular:
+#'   \item Nucleo_o_diseminado:
+#'   \item TIPO:    Municipios, Entidad Singular, Nucleos ....
+#' }
+#'
+#' @examples
+#' \dontrun{
+#'  df <- INE_padron_10_17
+#' }
+#'
+"INE_padron_10_17"
+
---
+++ data-raw/script_padron_continuo_v0.R
@@ -0,0 +1,126 @@
+#- me da x mirar datos del padron Continuo y acabao bajandome todos
+
+library(stringr)
+library(readr)
+library(tidyverse)
+library(personal.pjp)   #- personal packages
+library(readxl)
+library(readr)
+#devtools::install_github("ropensci/writexl")  #- lo instalamos
+library(writexl)                              #- lo cargamos en memoria
+library(stringi)
+
+
+
+#------------------------------- SI, ya es PADRON CONTINUO
+#- son microdatos, cada registro tiene 102 caracteres para conformar 5 varibles
+
+
+#- DICCIONARIO
+dicc_1 <- read_excel("./data-raw/datos_Padron_Continuo/DisenioRegistro.xls", skip = 3, sheet = "dicc_pjp")   #- las def. de las variables
+dicc_2 <- read_excel("./data-raw/datos_Padron_Continuo/DisenioRegistro.xls", sheet = "dicc_pjp_2")   #- las def. de las variables
+
+#- files with data
+
+aa <- list.files("./data-raw/datos_Padron_Continuo/datos/") #- un vector con los nombres de los ficheros de datos
+#- reading the data   #- only basers use list.files ; usa dir()
+
+df <-  read_fwf(paste0("./data-raw/datos_Padron_Continuo/datos/",aa[1]), fwf_widths(c(102)), locale = locale(encoding = "WINDOWS-1252"))   %>%  mutate(anyo = str_extract(aa[1], "(........)$")) #- el primer archivo
+for (ii in 2:length(aa)){
+  bb <- read_fwf(paste0("./data-raw/datos_Padron_Continuo/datos/",aa[ii]), fwf_widths(c(102)), locale = locale(encoding = "WINDOWS-1252"))  %>%  mutate(anyo = str_extract(aa[ii], "(........)$"))
+  df <- bind_rows(df, bb)
+}
+rm(aa,bb,ii)
+#--------- OK, leidos
+df_copy <- df   #- 1.219.020 registros
+#------------
+
+
+#-- sacar codigos
+df <- df %>% mutate(anyo = str_extract(anyo, "^(....)")) #- ok anyo está ok
+#df <- df %>% mutate(Code_unidad_poblacional = str_extract(anyo, "^(....)")) #- ok anyo está ok
+
+for (ii in 1:5){
+  df <- df %>% mutate(!!as.character(dicc_1[ii,1]) := substr(X1, dicc_1[ii,3], dicc_1[ii,4]))            #- sexo: 1= H, 6 = M
+}
+df <- df %>% map(str_trim, side = "both") %>% as_tibble() #- quito espacios en blanco, puede q haya
+
+for (ii in 1:5){
+  df <- df %>% mutate(!!as.character(dicc_2[ii,1]) := substr(Code_unidad_poblacional, dicc_2[ii,3], dicc_2[ii,4]))            #- sexo: 1= H, 6 = M
+}
+
+#-- arreglar
+df <- df %>% map(str_trim, side = "both") %>% as_tibble() #- quito espacios en blanco, puede q haya
+#df <- df %>% map(stringi::stri_enc_toutf8) %>% as_tibble() #- al final lo del encoding lo hice arriba
+
+
+# https://es.wikipedia.org/wiki/Entidad_singular_de_poblaci%C3%B3n
+#Las entidades y los núcleos de población están codificados por el Instituto Nacional de Estadística desde el año 1981. Este código está formado por 11 dígitos: (Hay mas explicacion)
+#Los códigos se restablecieron en el año 1991 siguiendo el orden alfabético de los núcleos de población dentro de cada entidad singular. A las entidades de nueva creación se les asigna un código correlativo al último existente, y no se reutilizan los de las que desaparecen. Los ayuntamientos deben revisar, al menos una vez al año, la relación de entidades y núcleos de población, y enviarla al Instituto Nacional de Estadística, que las publica anualmente.
+
+# Esta relación sirve de base para la confección del padrón municipal de habitantes.
+
+#- variable que señalice que es cada row (municipio, colectivo etc...
+aa <- df %>% filter(anyo == "2017") %>% filter(Provincia == 44) %>% filter(Municipio == 177) #- PANCRUDO
+
+df <- df %>% mutate(TIPO = "44")
+
+df <- df %>% mutate(TIPO = if_else(str_detect(Code_unidad_poblacional, "(000000)$"), "Municipio", TIPO)) #- Marco municipios
+df <- df %>% mutate(TIPO = if_else(Entidad_colectiva != "00", "Entidad colectiva", TIPO))                #- marco Entidad Colectiva
+df <- df %>% mutate(TIPO = if_else(Entidad_singular != "00", "Entidad singular", TIPO))                  #- marco Entidad singular
+df <- df %>% mutate(TIPO = if_else(Nucleo_o_diseminado != "00" & Nucleo_o_diseminado != "99", "Nucleos", TIPO))    #- Nucleos
+df <- df %>% mutate(TIPO = if_else(str_detect(Code_unidad_poblacional, "(99)$"), "Diseminados", TIPO))             #- Diseminados
+
+
+
+
+
+
+
+
+#------------------------------------------------------------ YA CALCULOS YA
+aa <- df %>% filter(str_detect(X1 ,"PORTALRUBIO"))
+
+aa <- df %>% filter(anyo == "2017") %>% filter(Provincia == 44) %>% filter(Municipio == 177) #- PANCRUDO
+
+
+#- municipios en 2017 (EC=00, ES=00 y NUC=00)
+aa <- df %>% filter(anyo == "2017")  %>% filter(str_detect(Code_unidad_poblacional, "(000000)$"))
+#- nº de municipios por año
+aa <- df %>% filter(str_detect(Code_unidad_poblacional, "(000000)$")) %>% group_by(anyo) %>% summarise(NN = n())
+
+
+
+#- Entidades Colectivas (EC ≠ 00, ES = 00 y NUC = 00)
+aa_colectivas <- df %>% filter(anyo == "2017") %>% filter(Entidad_colectiva != "00" ) %>% filter(str_detect(Code_unidad_poblacional, "(0000)$"))
+
+#- nº de entidades colectivas por año.
+aa_colectivas <- df %>% filter(Entidad_colectiva != "00" ) %>% filter(str_detect(Code_unidad_poblacional, "(0000)$")) %>% group_by(anyo) %>% summarise(NN = n())
+
+
+
+#- Entidades Singulares (ES != 00 y NUC = 00)
+
+aa_singulares <- df %>% filter(anyo == "2017") %>% filter(Entidad_singular != "00" ) %>% filter(str_detect(Code_unidad_poblacional, "(00)$"))
+
+#- Entidades singulares deshabitados
+
+aa_0 <- df %>% filter(anyo == "2017") %>% filter(Entidad_singular != "00" ) %>% filter(str_detect(Code_unidad_poblacional, "(00)$")) %>% filter(Poblacion_Total == 0)
+
+
+aa_0 <- df %>% filter(anyo == "2017") %>% filter(Entidad_singular != "00" ) %>% filter(str_detect(Code_unidad_poblacional, "(00)$")) %>% filter(Poblacion_Total != 0 & Poblacion_M == 0)
+
+
+aa_0 <- df %>% filter(anyo == "2017") %>% filter(Entidad_singular != "00" ) %>% filter(str_detect(Code_unidad_poblacional, "(00)$")) %>% filter(Poblacion_Total != 0 & Poblacion_H == 0)
+
+
+
+#- Nucleo o diseminado. Nucleos: NUC !=0  Diseminados: NUC = 99
+#- (siempre pertenecen a una entidad singular), los núcleos se identifican por el código NUC ≠ 00 y los diseminados por el código NUC = 99
+
+aa_nucleos <- df %>% filter(anyo == "2017")  %>% filter(Nucleo_o_diseminado != "00" ) %>% filter(Nucleo_o_diseminado != "99")
+aa_diseminados <- df %>% filter(anyo == "2017")  %>% filter(str_detect(Code_unidad_poblacional, "(99)$"))
+
+
+
+aa <- df %>% filter(anyo == "2010") %>% filter(Provincia == "01") %>% filter(Municipio == "001") #- PANCRUDO
